f207b98f61a8bd01965ab7d32d7c38e2d10a1bce
Tam Nguyen <tam.nguyen9779@gmail.com>
2017-10-01 20:43:04 -0500

    Assignment 2

---
+++ Assignment2/TextProcessingBook.hs
@@ -0,0 +1,105 @@
+{-  CSci 450/502, Org. of Programming Languages, Fall 2017
+    H. Conrad Cunningham, Assignment #2
+
+1234567890123456789012345678901234567890123456789012345678901234567890
+
+2000-02-15: Original version
+2007-02-18: Add import of Haskell 98 Char module
+2017-09-12: Update for Haskell 2010, put in module, use Data.Char
+
+-}
+
+module TextProcessingBook
+-- add needed exports
+where
+  
+import Data.Char
+
+{-  BEGIN function definitions from section 7.6 of
+    S. Thompson. _Haskell: The Craft of Functional Progamming_
+    Third Edition, Addison Wesley, 2011.
+-}
+
+-- Type synonyms for words and lines. (Prime added to textbook version
+-- to avoid name clash.)
+type Word' = String
+type Line' = [Word']
+
+-- The "whitespace" characters.
+whitespace :: String
+whitespace = ['\n','\t',' ']
+
+-- Get a word from the front of a string.
+getWord :: String -> String
+getWord []                = [] 
+getWord (x:xs) 
+    | elem x whitespace   = []
+    | otherwise           = x : getWord xs
+    
+-- Drop the first word of a string.
+dropWord :: String -> String
+dropWord []               = []
+dropWord (x:xs) 
+    | elem x whitespace   = (x:xs)
+    | otherwise           = dropWord xs
+
+-- Remove the whitespace character(s) from the front of a string.
+dropSpace :: String -> String
+dropSpace []              = []
+dropSpace (x:xs) 
+    | elem x whitespace   = dropSpace xs
+    | otherwise           = (x:xs)
+
+-- Split a string into words.
+splitWords :: String -> [Word']
+splitWords st = split (dropSpace st)
+
+split :: String -> [Word']
+split [] = []
+split st = (getWord st) : split (dropSpace (dropWord st))
+
+-- Split into lines of length at most lineLen.
+lineLen :: Int
+-- lineLen = 80
+lineLen = 10   -- for testing purposes
+
+-- Get a line from a list (name changed from Thompson book),
+getLine2 :: Int -> [Word'] -> Line'
+getLine2 len []           = []
+getLine2 len (w:ws)
+    | length w <= len     = w : restOfLine  
+    | otherwise           = []
+    where
+        newlen      = len - (length w + 1)
+        restOfLine  = getLine2 newlen ws
+
+-- Get a line from a list, with error repaired.
+-- If a word is longer than the total line length, textbook solution
+-- for splitLines did not terminate.  The repair is to make getLine
+-- keep the entire word as a line by itself.
+
+getLine3 :: Int -> [Word'] -> Line'
+getLine3 len []      = []
+getLine3 len (w:ws)
+    | lenword <= len = w : getLine3 newlen ws
+    | len == lineLen = [w]  -- word too long, line by itself
+    | otherwise      = []
+    where
+        lenword = length w
+        newlen  = len - (lenword + 1)
+
+-- Drop the first line from a list of words.
+dropLine :: Int -> [Word'] -> Line'
+dropLine = dropLine    -- DUMMY DEFINITION
+
+-- Split into lines. (Changed to use getLine3.)
+splitLines :: [Word'] -> [Line']
+splitLines [] = []
+splitLines ws = getLine3 lineLen ws
+                    : splitLines (dropLine lineLen ws)
+
+-- Fill a text string into lines.
+fill :: String -> [Line']
+fill = splitLines . splitWords
+
+-- END function definitions from Thompson textbook
---
+++ Assignment2/homework2.hs
@@ -0,0 +1,59 @@
+-- CSCI 450
+-- Homework 2
+-- Tam Nguyen
+
+import TextProcessingBook
+
+-- 7.27
+-- Define the function dropLine specified in the text
+--dropLine :: String -> String
+--dropLine [] = []
+--dropLine (x:xs)
+--	| elem x ['\n'] = dropSpace (x:xs)
+--	| otherwise 	= dropLine xs
+
+-- 7.28
+-- Give a definition of the function
+-- joinLine :: Line -> String
+-- which turns a line into printable form. For example:
+-- joinLine ["dog", "cat"] = "dog cat"
+
+-- 7.29
+-- Using the fuction joinLine, or otherwise, define the function
+-- joinLines :: [Line] -> String
+
+-- 7.30
+-- In this case study we have defined separate 'take' and 'drop' functions for words and lines.
+-- Redesign the program so that it uses 'split' functions - like the prelude function splitAt - instead
+
+-- 7.31
+-- [Harder] Modify the function joinLine so that it justifies the line to length lineLen
+-- by adding the appropriate number of spaces between the words.
+
+-- 7.32
+-- Design a function:
+-- wc :: String -> (Int, Int, Int)
+-- which when given a text string returns the number of characters, words, and lines in the string.
+-- The end of a line in the string is signalled by the newline character '\n'.
+-- Design a similar function:
+-- wcFormat :: String -> (Int, Int, Int)
+-- which returns the same statistics for the text after it has been filled.
+
+-- 7.33
+-- Define a function:
+-- isPalin :: String -> Bool
+-- which tests whether a string is a palindrome - that is whether it is the same read both backwards and forwards.
+-- An example is the string: Madam I'm Adam
+-- Note the punctuation and white space are ignored in the test, and that no distinction is made between capital and small letters.
+-- You might first like to develop a test which simply tests the string is exactly the same backwards and forwards,
+-- and only afterwards take account of punctation and capital letters.
+
+-- 7.34
+-- [Harder] Design a function:
+-- subst :: String -> String -> String -> String
+-- so that
+-- subst oldSub newSub st
+-- is the result of replacing the first occurence in st of the substring oldSub by the substring newSub.
+-- For instance:
+-- subst "much " "tall " "How much is that?" = "How tall is that?"
+-- If the substring oldSub does not occur in st, the result should be st.
